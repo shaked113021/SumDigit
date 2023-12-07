61a1f264b3b1e66064e218817186030db67f28d4
Shaked Cohen <27896110+shaked113021@users.noreply.github.com>
2023-12-07 14:18:55 +0200

    changed name of sum to digits sum to avoid collision with built-in sum function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 input_num = int(input("Input digits to sum: "))
 
-sum = 0
+digits_sum = 0
 current_num = input_num
 while current_num != 0:
-    sum += current_num % 10
+    digits_sum += current_num % 10
     current_num = current_num // 10
 
-print("The sum of digits:", sum)
+print("The sum of digits:", digits_sum)
